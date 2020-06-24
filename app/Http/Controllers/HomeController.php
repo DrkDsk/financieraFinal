4e37d669522612dcbadd622a1f318e1ae5c05fed
DrkDsk <joseaph.1998@gmail.com>
2020-06-24 03:04:29 -0500

    Número de Clientes en DashBoard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Client;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $numeroClients = Client::all()->count();
+        return view('home',[
+            'numeroClientes' => $numeroClients
+        ]);
     }
 }
