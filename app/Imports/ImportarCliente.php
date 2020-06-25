d8f3bf0a93f5ad0827bd46d5f727b59a39f89508
DrkDsk <joseaph.1998@gmail.com>
2020-06-24 19:00:04 -0500

    Importar Clientas

--- app/Imports/ImportarCliente.php
+++ app/Imports/ImportarCliente.php
@@ -9,11 +9,6 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class ImportarCliente implements ToModel
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
     public function model(array $row)
     {
         return new Client([
