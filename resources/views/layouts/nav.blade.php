183e55bffdb83e024fab7711955d88bf849f7c5d
drkdsk <joseaph.1998@gmail.com>
2021-02-02 00:36:50 -0600

    barra de navegación modificada

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -12,43 +12,33 @@
 </head>
 <body>
     <div id="nav">
-        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
-        <!--
+        <nav class="navbar navbar-dark navbar-expand-lg bg-dark flex-md-nowrap shadow">
+        
         <a style="margin-left:2em;" class="navbar-brand" href="{{url('/home')}}">Home</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        
         <div class="collapse navbar-collapse" id="navbarToggleExternalContent">
             <div>
                 <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="{{url('clients')}}">Clientes <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item active">
                     <a class="nav-link" href="{{url('loans')}}">Préstamos</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item active">
                     <a class="nav-link" href="{{url('payments')}}">Pagos</a>
                 </li>
                 </ul>
             </div>
             
         </div>
-        -->
+        
         </nav>
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('bottom-js')
 </body>
 </html>
