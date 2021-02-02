266c1652ecfebf5b78e25484fb22227db7815a1f
drkdsk <joseaph.1998@gmail.com>
2021-02-02 01:00:34 -0600

    edit modificado

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -1,63 +1,69 @@
-@extends('layouts.app')
+@extends('layouts.nav')
 
 @section('content')
-<div class="row">
-    <div class="col-md-6 mx-auto">
-        <div class="card">
-            <div class="card-header">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h3 class="mb-0">{{__('Edit Client')}}</h3>
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <h3 class="mb-0">{{__('Edit Client')}}</h3>
+                            </div>
+                            <div>
+                                <a href="{{route('clients.index')}}" class="btn btn-danger">
+                                    {{__('Cancel')}}
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <a href="{{route('clients.index')}}" class="btn btn-danger">
-                            {{__('Cancel')}}
-                        </a>
+                    <div class="card-body">
+                        <form action="{{route('clients.update',$client)}}" method="POST">
+                            @csrf @method('PATCH')
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="name">{{__('Name')}}</label>
+                                    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{$client->name}}">
+                                    @error('phone')
+                                        <div class="invalid-feedback">
+                                            {{$message}}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="phone">{{__('Phone')}}</label>
+                                    <input type="text" name="phone" id="phone" class="form-control @error('phone') is-invalid @enderror" value="{{$client->phone}}">
+                                    @error('phone')
+                                        <div class="invalid-feedback">
+                                            {{$message}}
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group form-row">
+                                <div class="col-md-12">
+                                    <label for="address">{{__('Address')}}</label>
+                                    <input type="text" name="address" id="address" class="form-control @error('address') is-invalid @enderror" value="{{$client->address}}">
+                                    @error('address')
+                                        <div class="invalid-feedback">
+                                            {{$message}}
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-success btn-lg">{{__('Edit')}}</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
-            <div class="card-body">
-                <form action="{{route('clients.update',$client)}}" method="POST">
-                    @csrf @method('PATCH')
-                    <div class="form-group form-row">
-                        <div class="col-md-6">
-                            <label for="name">{{__('Name')}}</label>
-                            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{$client->name}}">
-                            @error('phone')
-                                <div class="invalid-feedback">
-                                    {{$message}}
-                                </div>
-                            @enderror
-                        </div>
-                        <div class="col-md-6">
-                            <label for="phone">{{__('Phone')}}</label>
-                            <input type="text" name="phone" id="phone" class="form-control @error('phone') is-invalid @enderror" value="{{$client->phone}}">
-                            @error('phone')
-                                <div class="invalid-feedback">
-                                    {{$message}}
-                                </div>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group form-row">
-                        <div class="col-md-12">
-                            <label for="address">{{__('Address')}}</label>
-                            <input type="text" name="address" id="address" class="form-control @error('address') is-invalid @enderror" value="{{$client->address}}">
-                            @error('address')
-                                <div class="invalid-feedback">
-                                    {{$message}}
-                                </div>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-success btn-lg">{{__('Edit')}}</button>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
