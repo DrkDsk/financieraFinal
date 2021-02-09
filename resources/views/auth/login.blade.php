3f3d1fc744c4d237bb6246e9835c16fd2d786189
drkdsk <joseaph.1998@gmail.com>
2021-02-09 16:44:17 -0600

    identado de vista

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,31 +18,32 @@
                 </div>
                 <div class="col-md-7">
                     <div class="card-body">
-                    <div class="brand-wrapper">
-                        <img src="assets/images/money.svg" alt="logo" class="logo">
-                    </div>
-                    <p class="login-card-description">Iniciar Sesión</p>
-                    <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                        <div class="form-group">
-                            <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email">
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                        <div class="brand-wrapper">
+                            <img src="assets/images/money.svg" alt="logo" class="logo">
                         </div>
-                        <div class="form-group mb-4">
-                            <label for="password" class="sr-only">Password</label>
-                            <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Contraseña">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Login">
+                        <p class="login-card-description">Iniciar Sesión</p>
+                        <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                            <div class="form-group">
+                                <label for="email" class="sr-only">Email</label>
+                                <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="password" class="sr-only">Password</label>
+                                <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Contraseña">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Login">
                         </form>
                         <a href="{{ route('password.request')}}" class="forgot-password-link">Olvidaste tu Contraseña?</a>
                         <p class="login-card-footer-text">No tienes Cuenta? <a href="{{route('register')}}" class="text-reset">Registrate Aquí</a></p>
