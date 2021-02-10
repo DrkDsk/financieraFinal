c5e465bf72d142a5a69cf60412cfe247faaf5f49
drkdsk <joseaph.1998@gmail.com>
2021-02-10 12:29:21 -0600

    rutas agregadas

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -12,6 +12,7 @@
 <body>
     <div id="nav">
         <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+            <a class="navbar-brand" href="{{url('/')}}">Financiera</a>
             <a class="navbar-brand" href="{{url('/home')}}">Inicio</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -22,11 +23,11 @@
                     <li class="nav-item active">
                         <a class="nav-link" href="{{url('clients')}}">Clientes <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="{{url('loans')}}">Préstamos <span class="sr-only">(current)</span></a>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{url('loans')}}">Préstamos <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="{{url('payments')}}">Pagos</a>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{url('payments')}}">Pagos</a>
                     </li>
                 </ul>
                 <form action="{{url('logout')}}" method="POST">
