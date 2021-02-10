593e6e19ef3ecaf4cc2355b8767267154fd69545
drkdsk <joseaph.1998@gmail.com>
2021-02-10 12:26:49 -0600

    numero de prestamos y pagos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,29 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Client;
+use App\Models\Loan;
+use App\Models\Payment;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $numeroClients = Client::all()->count();
+        $numeroPrestamos = Loan::all()->count();
+        $numeroPagos = Payment::all()->where('monto_recibido','>','0')->count();
         return view('home',[
-            'numeroClientes' => $numeroClients
+            'numeroClientes' => $numeroClients,
+            'numeroPrestamos' => $numeroPrestamos,
+            'numeroPagos' => $numeroPagos
         ]);
     }
 }
